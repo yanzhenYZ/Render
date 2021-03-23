dd7c4093caef38bfbb0f971f279162f0ae6f6508
yanzhen <sdyzsdut@163.com>
2021-03-23 18:22:10 +0800

    i420 bug

--- MetalRender/Render/YZRenderViewController.m
+++ MetalRender/Render/YZRenderViewController.m
@@ -17,8 +17,6 @@
  4. Layer BGRA nv12 show
   1. BGRA
   2. NV21
- 
- todo
   3. I420
  */
 @interface YZRenderViewController ()<YZRenderCaptureDelegate>
@@ -26,9 +24,13 @@
 
 @property (nonatomic, strong) YZLayerView *layerView;
 @property (nonatomic, strong) YZRenderMTKView *mtkView;
+
+@property (nonatomic, assign) CGSize size;
 @end
 
-@implementation YZRenderViewController
+@implementation YZRenderViewController {
+    CVPixelBufferRef _pixelBuffer;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -66,14 +68,90 @@
 #pragma mark - YZRenderCaptureDelegate
 -(void)capture:(YZRenderCapture *)capture pixelBuffer:(CVPixelBufferRef)pixelBuffer {
 //    NSLog(@"todo");
+#if 1
+    [self testI420PixelBuffer:pixelBuffer];
+#else
     [_layerView displayVideo:pixelBuffer];
-    
-
     [_mtkView displayVideo:pixelBuffer];
+#endif
 }
 
--(void)viewWillAppear:(BOOL)animated {
-    [super viewWillAppear:animated];
-    //self.navigationController.navigationBarHidden = YES;
+
+- (void)testI420PixelBuffer:(CVPixelBufferRef)pixelBuffer {
+    size_t width = CVPixelBufferGetWidth(pixelBuffer);
+    size_t height = CVPixelBufferGetHeight(pixelBuffer);
+    [self newDealTextureSize:CGSizeMake(width, height)];
+    if (!_pixelBuffer) {
+        NSLog(@"error---NO");
+        return;
+    }
+    CVPixelBufferLockBaseAddress(pixelBuffer, 0);
+    size_t yWidth = CVPixelBufferGetWidthOfPlane(pixelBuffer, 0);
+    size_t yheight = CVPixelBufferGetHeightOfPlane(pixelBuffer, 0);
+    int8_t *yBuffer = CVPixelBufferGetBaseAddressOfPlane(pixelBuffer, 0);
+    size_t yBytesPow = CVPixelBufferGetBytesPerRowOfPlane(pixelBuffer, 0);
+    //NSLog(@"___1234:%d:%d:%d", yWidth, yheight, yBytesPow);
+    
+    //size_t uvWidth = CVPixelBufferGetWidthOfPlane(pixelBuffer, 1);
+    size_t uvheight = CVPixelBufferGetHeightOfPlane(pixelBuffer, 1);
+    int8_t *uvBuffer = CVPixelBufferGetBaseAddressOfPlane(pixelBuffer, 1);
+    size_t uvBytesPow = CVPixelBufferGetBytesPerRowOfPlane(pixelBuffer, 1);
+    //NSLog(@"___1234:%d:%d:%d", uvWidth, uvheight, uvBytesPow);
+    CVPixelBufferUnlockBaseAddress(pixelBuffer, 0);
+    int8_t *uBuffer = malloc(uvBytesPow * uvheight / 2);
+    int8_t *vBuffer = malloc(uvBytesPow * uvheight / 2);
+    
+    for (int i = 0; i < uvBytesPow * uvheight / 2; i++) {
+        uBuffer[i] = uvBuffer[2*i];
+        vBuffer[i] = uvBuffer[2*i+1];
+    }
+    //todo 绿边
+    CVPixelBufferLockBaseAddress(_pixelBuffer, 0);
+    int8_t *yB = CVPixelBufferGetBaseAddressOfPlane(_pixelBuffer, 0);
+    int8_t *uB = CVPixelBufferGetBaseAddressOfPlane(_pixelBuffer, 1);
+    int8_t *vB = CVPixelBufferGetBaseAddressOfPlane(_pixelBuffer, 2);
+    memcpy(yB, yBuffer, width * height);
+    memcpy(uB, uBuffer, width * height / 4);
+    memcpy(vB, vBuffer, width * height / 4);
+    CVPixelBufferUnlockBaseAddress(_pixelBuffer, 0);
+    
+    [_layerView displayVideo:_pixelBuffer];
+    
+    free(uBuffer);
+    free(vBuffer);
+}
+
+
+- (void)newDealTextureSize:(CGSize)size {
+    if (!CGSizeEqualToSize(_size, size)) {
+        if (_pixelBuffer) {
+            CVPixelBufferRelease(_pixelBuffer);
+            _pixelBuffer = nil;
+        }
+        _size = size;
+    }
+    
+    if (_pixelBuffer) { return; }
+    NSDictionary *pixelAttributes = @{(NSString *)kCVPixelBufferIOSurfacePropertiesKey:@{}};
+    CVReturn result = CVPixelBufferCreate(kCFAllocatorDefault,
+                                            _size.width,
+                                            _size.height,
+                                            kCVPixelFormatType_420YpCbCr8Planar,
+                                            (__bridge CFDictionaryRef)(pixelAttributes),
+                                            &_pixelBuffer);
+    if (result != kCVReturnSuccess) {
+        NSLog(@"YZBGRAPixelBuffer to create cvpixelbuffer %d", result);
+        return;
+    }
+    size_t count = CVPixelBufferGetPlaneCount(_pixelBuffer);
+    size_t y = CVPixelBufferGetBytesPerRowOfPlane(_pixelBuffer, 0);
+    size_t u = CVPixelBufferGetBytesPerRowOfPlane(_pixelBuffer, 1);
+    size_t v = CVPixelBufferGetBytesPerRowOfPlane(_pixelBuffer, 2);
+    
+//    size_t y = CVPixelBufferGetHeightOfPlane(_pixelBuffer, 0);
+//    size_t u = CVPixelBufferGetHeightOfPlane(_pixelBuffer, 1);
+//    size_t v = CVPixelBufferGetHeightOfPlane(_pixelBuffer, 2);
+    
+    NSLog(@"____%d:%d:%d", y, u, v);
 }
 @end
